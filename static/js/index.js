3b3b0cbc33207b66333c7275b2a3e43f7821453c
Julisa23 <80276992+Julisa23@users.noreply.github.com>
2021-03-29 14:40:26 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,39 +1,43 @@
 //https://www.eclipse.org/paho/clients/js/
-//
+//funciones para botones
 //function LED1_On() {
 	//alert("led on");
 	//console.log("led on");
+//	message = new Paho.MQTT.Message("ON");va
+  //      message.destinationName = "julyvelasco19@gmail.com/led1";
+    //    client.send(message);
+	
 	//document.getElementById("sensor").innerHTML="led on";
   
 //}
 //function LED1_Off(){	
 	//alert("led off");
 	//console.log("led off");
+//	message = new Paho.MQTT.Message("OFF");
+ //       message.destinationName = "julyvelasco19@gmail.com/led1";
+  //      client.send(message);
 	//document.getElementById("sensor").innerHTML="led off";
-// }
-//
+//}
 
-var btn=document.getElementById('btn'),contador=0;
+// funcion  para encender y apagar led con un solo botón
+
+var btn=document.getElementById('btn'), contador=0;
 function cambio()
 { if (contador==0)
 	{
 	message = new Paho.MQTT.Message("ENCENDER");
-	message.destinationName= "julyvelasco19@gmail.com/led1";
+	message.destinationName = "julyvelasco19@gmail.com/led1";
 	client.send(message);
 	contador=1;
 	}
-else
+ else
 	{
 	message = new Paho.MQTT.Message("APAGAR");
-	message.destinationName= "julyvelasco19@gmail.com/led1";
+	message.destinationName = "julyvelasco19@gmail.com/led1";
 	client.send(message);
 	contador=0;
 	}
 }
-
-
-
-
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
@@ -58,7 +62,7 @@ else
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("julyvelasco19@gmail.com/test");
+    client.subscribe("julyvelasco19@gmail.com/led");
     message = new Paho.MQTT.Message("hola desde la web");
     message.destinationName = "julyvelasco19@gmail.com/led1";
     client.send(message);
@@ -80,83 +84,17 @@ else
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
+	  //comando para poner el sensor desde esp32
+	  document.getElementById("sensor").innerHTML=message.payloadString;
+	  if(message.payloadString==='Encendido'){
+		 document.getElementById("imagen").src="http://www.clker.com/cliparts/M/h/R/9/8/H/red-led-on-md.png";
+	  } else if(message.payloadString==='Apagado'){
+		 document.getElementById("imagen").src="http://www.clker.com/cliparts/D/M/r/s/n/P/led-red-off-md.png";
+ 		
+	  }
+	  if(message.payloadString==='Encendido'){
+	  	document.getElementById("btn").innerHTML="Apagar";
+	  } else if(message.payloadString==='Apagado'){
+		document.getElementById("btn").innerHTML="Encender";
+	  }
   }
-  
-
-var btn=document.getElementById('btn'),contador=0;
-
-function cambio()
-
-{ if (contador==0)
-
-{
-
-message = new Paho.MQTT.Message("ENCENDER");
-
-message.destinationName= "julyvelasco19@gmail.com/led1";
-
-client.send(message);
-
-contador=1;
-
-}
-
-else
-
-{
-
-message = new Paho.MQTT.Message("APAGAR");
-
-message.destinationName= "julyvelasco19@gmail.com/led1";
-
-client.send(message);
-
-contador=0;
-
-}
-
-}
-// called when a message arrives function ... de Julisa Velasco
-
-function onConnectionLost(responseObject){
-
-    if (responseObject.errorCode!== 0) {
-
-        console.log("onConnectionLost:"+responseObject.errorMessage);
-
-    }
-
-}
-// called when a message arrives
-
- function onMessageArrived(message){
-
-   console.log("onMessageArrived:"+message.payloadString);
-
-//comando para poner el sensor desde esp32
-
-        document.getElementById("sensor").innerHTML=message.payloadString;
-
-        if(message.payloadString==='Encendido'){
-
-             document.getElementById("imagen").src="http://www.clker.com/cliparts/M/h/R/9/8/H/red-led-on-md.png";
-
-        } else if(message.payloadString==='Apagado'){
-
-             document.getElementById("imagen").src="http://www.clker.com/cliparts/D/M/r/s/n/P/led-red-off-md.png";
-
-
-
-        }
-
-         if(message.payloadString==='Encendido'){
-
-             document.getElementById("btn").innerHTML="Apagar";
-
-         } else if(message.payloadString==='Apagado'){
-
-             document.getElementById("btn").innerHTML="Encender";
-
-        }
-
-}
